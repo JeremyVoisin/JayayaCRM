169a28abf49ef51c073a894be04a00381aebc600
Jérémy Voisin <jeremy.voisin@jeyz.fr>
2015-03-12 23:21:19 +0100

    Removed everything

--- includes/config/config.js
+++
@@ -1,8 +0,0 @@
-exports.admin = "Admin";
-exports.password = "Password";
-exports.mail = "admin@domain.com";
-exports.tel = "0123456789";
-exports.fax = "1234567890";
-exports.statut = "Admin";
-exports.first_name = "Admin";
-exports.last_name = "Admin";
